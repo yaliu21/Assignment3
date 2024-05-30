d003a3d9330d80f0a21707a7f6087b5c190b24c0
yaliu21 <yaliu21@student.unimelb.edu.au>
2024-05-31 09:58:01 +1000

    Implement on the product page

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+function decreaseQuantity() {
+    var quantityInput = document.getElementById('quantity');
+    var currentQuantity = parseInt(quantityInput.value);
+    if (currentQuantity > 1) {
+        quantityInput.value = currentQuantity - 1;
+    }
+}
+
+function increaseQuantity() {
+    var quantityInput = document.getElementById('quantity');
+    var currentQuantity = parseInt(quantityInput.value);
+    quantityInput.value = currentQuantity + 1;
+}
