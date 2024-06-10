e9fbb2125d62339e16d3e9e0fd2d895a86f77b1d
yaliu21 <yaliu21@student.unimelb.edu.au>
2024-06-10 10:12:09 +1000

    Implement on checkout pages

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,25 @@
-function decreaseQuantity() {
-    var quantityInput = document.getElementById('quantity');
+// Function to decrease the quantity of a specific product
+function decreaseQuantity(productId) {
+    // Construct the input field ID
+    var quantityInputId = 'quantity-' + productId;
+    // Get the input element
+    var quantityInput = document.getElementById(quantityInputId);
     var currentQuantity = parseInt(quantityInput.value);
+
+    // Decrease the quantity, but don't go below 1
     if (currentQuantity > 1) {
         quantityInput.value = currentQuantity - 1;
     }
 }
 
-function increaseQuantity() {
-    var quantityInput = document.getElementById('quantity');
+// Function to increase the quantity of a specific product
+function increaseQuantity(productId) {
+    // Construct the input field ID
+    var quantityInputId = 'quantity-' + productId;
+    // Get the input element
+    var quantityInput = document.getElementById(quantityInputId);
     var currentQuantity = parseInt(quantityInput.value);
+
+    // Increase the quantity
     quantityInput.value = currentQuantity + 1;
 }
