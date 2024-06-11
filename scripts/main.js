9730e4bf515855e9b4ce4640595da736307b2eeb
yaliu21 <yaliu21@student.unimelb.edu.au>
2024-06-11 18:25:30 +1000

    Make the thumbnail clickable

--- scripts/main.js
+++ scripts/main.js
@@ -28,3 +28,19 @@ function addToCart() {
     //Remind users that the product has been added to the shopping cart
     alert('Product added to cart!');
 }
+
+//Switch to main image when clicking thumbnail
+document.addEventListener('DOMContentLoaded', function() {
+    // get all thumbnail
+    const thumbnails = document.querySelectorAll('.thumbnail');
+    // get main image
+    const mainImage = document.getElementById('main-image');
+
+    //check if is clicked
+    thumbnails.forEach(thumbnail => {
+        thumbnail.addEventListener('click', function() {
+            // change main iamge when click
+            mainImage.src = this.src;
+        });
+    });
+});
